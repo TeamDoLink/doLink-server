f634cb9b5fdc836d83e205ee208ef6c371aa48a1
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2025-11-23 16:28:09 +0900

    init commit

---
+++ src/main/java/com/doLink_server/DoLinkApplication.java
@@ -0,0 +1,13 @@
+package com.doLink_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doLink_server/DoLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doLink_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
