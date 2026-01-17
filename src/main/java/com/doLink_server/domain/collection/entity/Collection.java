bfded1957c2b307c7d2912627f33fad6cd826679
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2026-01-17 18:05:22 +0900

    chore: 모음제목 20글자 제약

---
+++ src/main/java/com/doLink_server/domain/collection/entity/Collection.java
@@ -0,0 +1,57 @@
+package com.doLink_server.domain.collection.entity;
+
+
+import com.doLink_server.global.common.BaseEntity;
+import com.doLink_server.global.enums.Category;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+
+/**
+ * 모음 테이블
+ */
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(name = "collection", schema = "talktodo")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Collection extends BaseEntity {
+
+    /**
+     * 모음 ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "collection_id")
+    private Long collectionId;
+
+    /**
+     * 모음 이름
+     */
+    @NotNull
+    @Size(max = 20)
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    /**
+     * 모음 카테고리
+     */
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false, length = 30)
+    private Category category;
+
+    /**
+     * 사용자 ID (UUID, BINARY 16)
+     */
+    @Column(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
+    private byte[] userId;
+
+
+
+
+}
